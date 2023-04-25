b2d4edff3ab85774a713307f7da6e924889ccf11
vibhormiet2009 <vibhor.agarwal@unicommerce.com>
2023-04-25 18:06:28 +0530

    tally

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class Main {
-``    private static final Logger logger = Logger.getLogger(Main.class);
+   private static final Logger logger = Logger.getLogger(Main.class);
     private static final long serialVersionUID = 1L;
     public Main() throws IOException, InterruptedException {
 
